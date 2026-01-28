4c460495fe250941cbae1629ba9a61765e4a8000
Do Xuan Tu <doxuantutlbt@gmail.com>
2026-01-28 19:51:16 +0700

    add spring security

--- src/main/java/com/example/laptopshop/LaptopshopAplication.java
+++ src/main/java/com/example/laptopshop/LaptopshopAplication.java
@@ -3,9 +3,14 @@ package com.example.laptopshop;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+// @SpringBootApplication
 @RestController
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
 public class LaptopshopAplication {
 
 	public static void main(String[] args) {
