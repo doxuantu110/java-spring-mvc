7926443a6f501b6109dcb4b5d41001a27d69ce7a
Do Xuan Tu <doxuantutlbt@gmail.com>
2026-01-31 10:28:24 +0700

    set up jsp

--- src/main/java/com/example/laptopshop/controller/UserController.java
+++ src/main/java/com/example/laptopshop/controller/UserController.java
@@ -21,7 +21,7 @@ public class UserController {
 
     public String getHomePage() {
         String test = this.userService.handleHello();
-        return "eric.html";
+        return "hello";
     }
 }
 
